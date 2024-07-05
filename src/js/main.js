41bd0efe88ca2ad946268ff215953128e7fe9a22
Szymon Bednorz <dsonyyy@gmail.com>
2024-07-05 10:37:59 +0200

    more responsive plot with d3js

--- src/js/main.js
+++ src/js/main.js
@@ -2,48 +2,44 @@ import _ from "lodash";
 import "../scss/styles.scss";
 import "bootstrap";
 import * as d3 from "d3";
+import * as fc from "d3fc";
 import Chart from "chart.js/auto";
 
-document.addEventListener("DOMContentLoaded", function () {
-  render();
-});
+// create some test data
+const prng = d3.randomNormal();
+
+const data = d3.range(1e3).map((d) => ({
+  x: prng(),
+  y: prng(),
+}));
+
+const x = d3.scaleLinear().domain([-5, 5]);
+const y = d3.scaleLinear().domain([-5, 5]);
+
+const area = fc
+  .seriesCanvasPoint()
+  .crossValue((d) => d.x)
+  .mainValue((d) => d.y)
+  .size(4);
+
+// create a d3fc-zoom that handles the mouse / touch interactions
+const zoom = fc.zoom().on("zoom", render);
+
+// the chart!
+const chart = fc
+  .chartCartesian(x, y)
+  .chartLabel("Canvas Zoom 1,000 Points")
+  .canvasPlotArea(area)
+  .decorate((sel) => {
+    // add the zoom interaction on the enter selection
+    // use selectAll to avoid interfering with the existing data joins
+    sel.enter().selectAll(".plot-area").call(zoom, x, y);
+    sel.enter().selectAll(".x-axis").call(zoom, x, null);
+    sel.enter().selectAll(".y-axis").call(zoom, null, y);
+  });
 
 function render() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  const container = document.getElementById("container");
-
-  // Create the SVG container.
-  const svg = d3.create("svg").attr("width", width).attr("height", height);
-
-  // Create random Gaussian variables for X and Y coordinates
-  const randomX = d3.randomNormal(1000 / 2, 80);
-  const randomY = d3.randomNormal(1000 / 2, 80);
-  const data = Array.from({ length: 1000 }, () => [randomX(), randomY()]);
-
-  // Add scatter plot points
-  const circle = svg
-    .selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("transform", (d) => `translate(${d})`)
-    .attr("r", 1.5);
-
-  svg.call(
-    d3
-      .zoom()
-      .extent([
-        [0, 0],
-        [width, height],
-      ])
-      .scaleExtent([1, 8])
-      .on("zoom", zoomed)
-  );
-
-  function zoomed({ transform }) {
-    circle.attr("transform", (d) => `translate(${transform.apply(d)})`);
-  }
-
-  // Append the SVG element.
-  container.append(svg.node());
+  d3.select("#container").datum(data).call(chart);
 }
+
+render();
