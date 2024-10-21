1d4bb3f56b15f5445c357cab5737ee98fcd84977
Szymon Bednorz <dsonyyy@gmail.com>
2024-10-21 15:19:40 +0200

    remove unused imports

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 import * as points from "./points.js";
 
-import "../styles.css";
-
 function enableLoadingScreen() {
   document.getElementById("loading").style.display = "block";
   document.getElementById("chart").style.display = "none";
